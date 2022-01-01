6dcbc66f6907a2f54f4184a36a0f57f3649f12e0
Your Name <you@example.com>
2022-01-01 04:25:52 +0100

    reversed angles so supplement is sent to arduino

--- python angle.py
+++ python angle.py
@@ -3,101 +3,107 @@ from cvzone.HandTrackingModule import HandDetector
 import time
 import threading
 import math
-#Resolution: 720 x 1280
 
-cap = cv2.VideoCapture(0)
-
-detector = HandDetector(maxHands=1, detectionCon=0.7)
+def index():
     
-def index(img):
+    lil, info = detector.findDistance(lmlist1[8], lmlist1[5])
     
-    lil, info, img = detector.findDistance(lmlist1[8], lmlist1[5], img)
+    lib, info = detector.findDistance(lmlist1[6], lmlist1[5])
     
-    lib, info, img = detector.findDistance(lmlist1[6], lmlist1[5], img)
-    
-    lit, info, img = detector.findDistance(lmlist1[8], lmlist1[6], img)
+    lit, info = detector.findDistance(lmlist1[8], lmlist1[6])
     
     
     index_angle = (lib**2 + lit**2 - lil**2) / (2 * lib * lit)
     
     index_angle = math.degrees(math.acos(index_angle))
+    
+    index_angle = 180 - index_angle
     return int(index_angle)
 
-def middle(img):
-    lml, info, img = detector.findDistance(lmlist1[12], lmlist1[9], img)
+def middle():
+    lml, info = detector.findDistance(lmlist1[12], lmlist1[9])
     
-    lmb, info, img = detector.findDistance(lmlist1[12], lmlist1[10], img)
+    lmb, info = detector.findDistance(lmlist1[12], lmlist1[10])
     
-    lmt, info, img = detector.findDistance(lmlist1[10], lmlist1[9], img)
+    lmt, info = detector.findDistance(lmlist1[10], lmlist1[9])
     
     
     middle_angle = (lmb**2 + lmt**2 - lml**2) / (2 * lmb * lmt)
 
     middle_angle = math.degrees(math.acos(middle_angle))
+    
+    middle_angle = 180 - middle_angle
     return int(middle_angle)
 
-def ring(img):
+def ring():
 
-    lrl, info, img = detector.findDistance(lmlist1[16], lmlist1[13], img)
+    lrl, info = detector.findDistance(lmlist1[16], lmlist1[13])
     
-    lrb, info, img = detector.findDistance(lmlist1[16], lmlist1[14], img)
+    lrb, info = detector.findDistance(lmlist1[16], lmlist1[14])
     
-    lrt, info, img = detector.findDistance(lmlist1[14], lmlist1[13], img)
+    lrt, info = detector.findDistance(lmlist1[14], lmlist1[13])
     
     
     ring_angle = (lrb**2 + lrt**2 - lrl**2) / (2 * lrb * lrt)
     
     ring_angle = math.degrees(math.acos(ring_angle))
+    
+    ring_angle = 180 - ring_angle
     return int(ring_angle)
 
-def pinky(img):
-    lpl, info, img = detector.findDistance(lmlist1[20], lmlist1[17], img)
+def pinky():
+    lpl, info = detector.findDistance(lmlist1[20], lmlist1[17])
     
-    lpb, info, img = detector.findDistance(lmlist1[20], lmlist1[18], img)
+    lpb, info = detector.findDistance(lmlist1[20], lmlist1[18])
     
-    lpt, info, img = detector.findDistance(lmlist1[18], lmlist1[17], img)
+    lpt, info = detector.findDistance(lmlist1[18], lmlist1[17])
     
     
     pinky_angle = (lpb**2 + lpt**2 - lpl**2) / (2 * lpb * lpt)
     
     pinky_angle = math.degrees(math.acos(pinky_angle))
+    
+    pinky_angle = 180 - pinky_angle
     return int(pinky_angle)
 
-def thumb(img):
-    ltl, info, img = detector.findDistance(lmlist1[4], lmlist1[2], img)
+def thumb():
+    ltl, info = detector.findDistance(lmlist1[4], lmlist1[2])
     
-    ltb, info, img = detector.findDistance(lmlist1[4], lmlist1[3], img)
+    ltb, info = detector.findDistance(lmlist1[4], lmlist1[3])
     
-    ltt, info, img = detector.findDistance(lmlist1[3], lmlist1[2], img)
+    ltt, info = detector.findDistance(lmlist1[3], lmlist1[2])
     
     
     thumb_angle = (ltb**2 + ltt**2 - ltl**2) / (2 * ltb * ltt)
     
     thumb_angle = math.degrees(math.acos(thumb_angle))
+    
+    thumb_angle = 180 - thumb_angle
     return int(thumb_angle)
 
-def data(img):
+def data():
     #77, 8, 15, 18, 29
     
-    thumbangle = int((thumb(img) * 60) / 180)
-    indexangle = int((index(img) * 130) / 180)
-    middleangle = int((middle(img) * 100) / 180)
-    ringangle = int((ring(img) * 130) / 180)
-    pinkyangle = int((pinky(img) * 100) / 180)
+    thumbangle = int((thumb() * 60) / 100)
+    indexangle = int((index() * 130) / 180)
+    middleangle = int((middle() * 100) / 180)
+    ringangle = int((ring() * 130) / 180)
+    pinkyangle = int((pinky() * 100) / 180)
     
     arduino_angles = [thumbangle, indexangle, middleangle, ringangle, pinkyangle]    
     print(arduino_angles)
-    print(str(thumb(img)) + ", " + str(index(img)) + ", " + str(middle(img)) + ", " + str(ring(img)) + ", " + str(pinky(img)))
-    time.sleep(0.5)                
+    print(str(thumb()) + ", " + str(index()) + ", " + str(middle()) + ", " + str(ring()) + ", " + str(pinky()))
+                
+  
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1, detectionCon=0.7)
 
-threading.Thread(target=data).start()    
-
+start = time.time()
 while True:
     success, img = cap.read()
-    #img = cv2.resize(img, (640, 420)) 
+ 
     hands, img = detector.findHands(img)
-    
-    #print('Resolution: ' + str(img.shape[0]) + ' x ' + str(img.shape[1]))
+
     if hands:
         hand1 = hands[0]
         lmlist1 = hand1["lmList"]
@@ -105,8 +111,12 @@ while True:
         cp1 = hand1["center"]
         HandType = hand1["type"]
         
-        data(img)
-        #print(str(thumb(img)) + ", " + str(index(img)) + ", " + str(middle(img)) + ", " + str(ring(img)) + ", " + str(pinky(img)))
+    
+
+        current_time = time.time()
+        if current_time - start >= 0.5:
+            data()
+            start = current_time
             
     cv2.imshow("Hand Processor", img)
     cv2.waitKey(1) 
